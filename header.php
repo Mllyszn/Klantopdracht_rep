618363f90e663504e8d36274703bbbd14ac243f1
Mllyszn <melvinlieuw121107@gmail.com>
2024-06-06 11:05:14 +0200

    nn

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tuinman Hogendijk</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="icon" href="favicon.ico" />
+</head>
+
+<body>
+    <div id="container">
+        <header id="header">
+            <div id="weblogo-container">
+                <a href="index.php">
+                    <img id="weblogo" src="assets/img/logo_ko.png" alt="weblogo">
+                </a>
+            </div>
+            <div id="headerbuttons">
+                <a class="headerbutton" href="core/dienstenpage.php">Diensten</a>
+                <a class="headerbutton" href="core/recensiespage.php">Recensies</a>
+                <a class="headerbutton" href="core/overmijpage.php">Over mij</a>
+                <a class="headerbutton" href="core/contactpage.php">Contact</a>
+            </div>
+        </header>
