a30a8ad5de7c6204f50c874073befa0b91d3fee0
230752 <230752@student.glu.nl>
2024-06-06 10:41:03 +0200

    header en footer af

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tuinman Hogendijk</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="icon" href="favicon.ico" />
+</head>
+
+<body>
+    <div id="container">
+        <header id="header">
+            <div id="weblogo-container">
+                <a href="index.php">
+                    <img id="weblogo" src="assets/img/logo_ko.png" alt="weblogo">
+                </a>
+            </div>
+            <div id="headerbuttons">
+                <a class="headerbutton" href="#diensten">Diensten</a>
+                <a class="headerbutton" href="#recensies">Recensies</a>
+                <a class="headerbutton" href="#overmij">Over mij</a>
+                <a class="headerbutton" href="#contact">Contact</a>
+            </div>
+        </header>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include ('header.php');
+?>
+<main>
+  <div class="nepimg">
+    <img src="assets/img/emptyimgT.jpg" alt="emptyImg">
+    <!-- <h3>(slideshow foto's) ^</h3> -->
+  </div>
+</main>
+<?php include ('footer.php'); ?>
