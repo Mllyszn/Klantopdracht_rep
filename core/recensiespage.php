1765660937e800f1bd166033e7ed7dde73c5bc25
Mllyszn <melvinlieuw121107@gmail.com>
2024-06-10 10:47:52 +0200

    ss

--- core/recensiespage.php
+++ core/recensiespage.php
@@ -1,11 +1,33 @@
+<<<<<<< Updated upstream
+=======
+<?php include ('../header.php') ?>
+
+>>>>>>> Stashed changes
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/recensieStyle.css">
     <title>Recensies</title>
 </head>
+
 <body>
-    
+    <div class="recensie">
+        <form action="recensiespage.php">
+            <label for="subject">Schrijf hier je recensie</label>
+            <textarea id="subject" name="subject" placeholder="Schrijf hier.." rows="4" cols="50"></textarea>
+
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
+
 </body>
+<<<<<<< Updated upstream
+=======
+<?php include ('../footer.php') ?>
+
+>>>>>>> Stashed changes
 </html>
