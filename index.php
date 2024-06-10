ba4197df399cbf57f864abe50be178e8dadfefd3
230752 <230752@student.glu.nl>
2024-06-10 11:02:01 +0200

    idk

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
-<?php
-include ('header.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<main>
-  <div class="nepimg">
-    <img src="assets/img/emptyimgT.jpg" alt="emptyImg">
-    <!-- <h3>(slideshow foto's) ^</h3> -->
-  </div>
-</main>
-<?php include ('footer.php'); ?>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Tuinman Hogendijk</title>
+  <link rel="icon" href="favicon.ico" />
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+  <nav>
+    <div id="container">
+      <header id="header">
+        <div id="weblogo-container">
+          <a href="../index.php">
+            <img id="weblogo" src="assets/img/logo_ko.png" alt="weblogo">
+          </a>
+        </div>
+        <div id="headerbuttons">
+          <a class="headerbutton" href="/core/dienstenpage.php">Diensten</a>
+          <a class="headerbutton" href="/core/recensiespage.php">Recensies</a>
+          <a class="headerbutton" href="/core/overmijpage.php">Over mij</a>
+          <a class="headerbutton" href="/core/contactpage.php">Contact</a>
+        </div>
+      </header>
+    </div>
+  </nav>
+
+  <main>
+    <div class="nepimg">
+      <img src="assets/img/emptyimgT.jpg" alt="emptyImg">
+      <!-- <h3>(slideshow foto's) ^</h3> -->
+    </div>
+  </main>
+  <?php include ('footer.php'); ?>
