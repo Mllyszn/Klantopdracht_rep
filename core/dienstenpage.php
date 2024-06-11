5b4de9a97cd4a8c8e5c3a80cde9d8bf16764ab44
Mllyszn <melvinlieuw121107@gmail.com>
2024-06-11 15:45:45 +0200

    dienstenpage

--- core/dienstenpage.php
+++ core/dienstenpage.php
@@ -14,17 +14,43 @@
     <div class="diensten">
         <div class="Tontwerp">
             <img src="/assets/img/icon-tuinontwerp.png" alt="Foto ontwerp">
+            <h4>Tuin ontwerp</H4>
+            <p>Een mooie tuin valt of staat bij een goed tuinontwerp.
+                Als het op ontwerpen aankomt, maakt Hendrik Hogendijk het verschil.
+            </p>
         </div>
         <div class="Taanleg">
             <img src="/assets/img/icon-tuinaanleg.png" alt="Foto aanleg">
+            <h4>Tuin aanleg</h4>
+            <p>
+                Het aanleggen van een tuin doen ik altijd vanuit dezelfde drive: ik wil er iets moois van maken.
+            </p>
         </div>
         <div class="Tonderhoud">
             <img src="/assets/img/icon-tuinonderhoud.png" alt="Foto-onderhoud">
+            <H4>Tuin onderhoud</H4>
+            <p>
+                Een mooie tuin verdient het juiste onderhoud.
+                Immers, goed onderhoud is een essentiële voorwaarde voor een tuin die het hele jaar door mooi blijft.
+            </p>
         </div>
         <div class="Zvijvers">
             <img src="/assets/img/icon-zwemvijvers.png" alt="Foto-vijvers">
+            <H4>Zwem vijvers</H4>
+            <p>
+                Zwemvijvers, is één van mjn grootste passies.
+                En gelukkig, ze winnen aan steeds meer populariteit, waardoor ik er veel ervaring mee heb opgebouwd.
+            </p>
         </div>
     </div>
+
+    <!-- <div class="container">
+        <div class="box">A</div>
+        <div class="box">B</div>
+        <div class="box">C</div>
+        <div class="box">D</div>
+    </div> -->
+
 </body>
 <?php include ('../footer.php') ?>
 
