1b4a3e634b7b75d094a89734e678a2225054bbd4
Mllyszn <melvinlieuw121107@gmail.com>
2024-06-11 13:23:49 +0200

    jpush

--- index.php
+++ index.php
@@ -10,12 +10,21 @@
 </head>
 
 <body>
-  <?php include ('header.php')?>
+  <?php include ('header.php') ?>
 
   <main>
     <div class="nepimg">
       <img src="assets/img/emptyimgT.jpg" alt="emptyImg">
       <!-- <h3>(slideshow foto's) ^</h3> -->
+    </div>
+    <div class="imgtext">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nunc tortor, fermentum id pretium id, fringilla
+        sed enim. Integer et ipsum eget mauris porttitor pretium semper a sapien. Vestibulum consequat dui sed purus
+        gravida fermentum. In ut mauris ut mi consectetur consectetur in id libero. Sed sodales pulvinar blandit. Nulla
+        sollicitudin, eros vitae blandit pharetra, est lorem vestibulum dui, nec sagittis quam justo id eros. Nulla sed
+        lacus tincidunt, maximus leo sed, lobortis arcu. Fusce pulvinar convallis cursus. Nunc eget efficitur nibh.
+        Donec euismod turpis sed tempus tristique. Quisque scelerisque sollicitudin massa a ornare. Fusce tempus ligula
+        massa, quis posuere.</p>
     </div>
   </main>
   <?php include ('footer.php'); ?>
