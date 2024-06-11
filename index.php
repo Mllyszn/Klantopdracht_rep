31bdba18324b402def38c41fab9cd41e7322270f
Mllyszn <melvinlieuw121107@gmail.com>
2024-06-11 13:38:54 +0200

    k

--- index.php
+++ index.php
@@ -17,6 +17,24 @@
       <img src="assets/img/emptyimgT.jpg" alt="emptyImg">
       <!-- <h3>(slideshow foto's) ^</h3> -->
     </div>
+<<<<<<< Updated upstream
+=======
+    <div class="inleiding">
+      <H4>Welkom bij de website van Hendrik Hogendijk, hovenier in de regio Utrecht, Zeist en de Bilt. </H4>
+
+      <div class="portret">
+        <img src="/assets/img/Hendrik_foto.png" alt="portret van Hendrik Hogendijk">
+      </div>
+
+        <p>De tuin is een belangrijke plek van de woning, waar je het liefst zoveel mogelijk tijd in doorbrengt. Bij
+        Hendrik Hogendijk Hoveniers vinden wij het daarom belangrijk dat iedereen zich thuis voelt in zijn of haar tuin.
+        Ik maak de tuin onderdeel van jouw ‘thuis’, door hem volledig op jouw wensen af te stemmen.
+
+        Met behulp van mooie, natuurlijke en duurzame producten en materialen creëer ik een tuin, die garant staat voor
+        een jarenlang plezierig buitenleven. Van een knusse veranda en een mooie vijver, tot een gezellig terras en een
+        kleurrijke bloemenborder: ik stop al mijn energie erin.</p>
+    </div>
+>>>>>>> Stashed changes
   </main>
   <?php include ('footer.php'); ?>
 </body>
